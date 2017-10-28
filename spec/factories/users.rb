6f937fef9792a23307c3d1e3f5d20afcbb289010
Matt DeVoe <matthew.devoe@gmail.com>
2017-10-28 12:12:40 -0600

    created model/migration for users

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    first_name "MyString"
+    last_name "MyString"
+    gender "MyString"
+    age 1
+    height 1
+    token "MyText"
+    refresh_token "MyText"
+  end
+end
