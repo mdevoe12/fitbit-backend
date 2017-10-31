c4a6d82cb0b153c83b2eef6e0ffecda348bfcc5c
Matt DeVoe <matthew.devoe@gmail.com>
2017-10-31 11:40:06 -0600

    redirect to front end with token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,20 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login!, only: [:create]
-
-
   skip_before_action :verify_authenticity_token
-
   protect_from_forgery :except => [:create]
 
   def create
-    redirect_to root_path
     code = params['code']
     FitbitTokenService.get_token(code)
     @user = User.find_or_create_from_auth(request.env['omniauth.auth'])
     @user.generate_auth_token
-    binding.pry
+    redirect_to "http://localhost:8080?token=#{@user.auth_token}"
     # FitbitApiService.get_sleep_info(@user)
+  end
+
+  def destroy
+    current_user.invalidate_auth_token
+    head :ok
   end
 
 end
