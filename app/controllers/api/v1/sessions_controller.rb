76d71e13de3c4ad6fb18e3b744a9ad5971a46ea5
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-01 16:55:22 -0600

    updated current_user method, confirmed destroy works

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,17 +1,9 @@
 class Api::V1::SessionsController < ApplicationController
 
-  skip_before_action :require_login!, only: [:create]
+  skip_before_action :require_login!, only: [:create, :destroy]
   skip_before_action :verify_authenticity_token
   protect_from_forgery :except => [:create]
 
-  def create
-    code = params['code']
-    FitbitTokenService.get_token(code)
-    @user = User.find_or_create_from_auth(request.env['omniauth.auth'])
-    binding.pry
-    @user.generate_auth_token
-    FitbitApiService.get_sleep_info(@user)
-  end
 
   def destroy
     current_user.invalidate_auth_token
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'keys', to: "keys#index"
+      delete 'logout', to: "sessions#destroy"
       # get '/auth/fitbit/callback', to: 'sessions#create'
       # get '/auth/fitbit', as: :fitbit_login
     end
