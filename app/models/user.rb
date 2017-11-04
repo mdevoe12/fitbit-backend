67116bff83e1af9566fa42cf884211117cbfef9f
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-04 10:44:19 -0600

    reworking account finding/creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,23 @@ class User < ApplicationRecord
   has_many :bodies
 
   def self.find_or_create_from_auth(auth)
-    user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid'])
-
-    user.first_name    = auth['extra']['raw_info']['user']['firstName']
-    user.last_name     = auth['extra']['raw_info']['user']['lastName']
-    user.gender        = auth['extra']['raw_info']['user']['gender']
-    user.age           = auth['extra']['raw_info']['user']['age']
-    user.height        = auth['extra']['raw_info']['user']['height']
-    user.height        = auth['extra']['raw_info']['user']['height']
-    user.height        = auth['extra']['raw_info']['user']['height']
-    user.token         = auth['credentials']['token']
-    user.refresh_token = auth['credentials']['refresh_token']
-
-    user.save
+    if User.exists?(:uid => auth['uid'])
+      user = User.find_by(provider: auth['provider'], uid: auth['uid'])
+    else
+      user = User.new
+      user.provider      = auth['provider']
+      user.uid           = auth['uid']
+      user.first_name    = auth['extra']['raw_info']['user']['firstName']
+      user.last_name     = auth['extra']['raw_info']['user']['lastName']
+      user.gender        = auth['extra']['raw_info']['user']['gender']
+      user.age           = auth['extra']['raw_info']['user']['age']
+      user.height        = auth['extra']['raw_info']['user']['height']
+      user.height        = auth['extra']['raw_info']['user']['height']
+      user.height        = auth['extra']['raw_info']['user']['height']
+      user.token         = auth['credentials']['token']
+      user.refresh_token = auth['credentials']['refresh_token']
+      user.save
+    end
     user
   end
 
