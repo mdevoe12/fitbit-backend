48f30a1a7fcd7af5ecd34e5709f2f631600d2633
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-04 11:43:02 -0600

    refactored and privatized create_new_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,23 +7,9 @@ class User < ApplicationRecord
 
   def self.find_or_create_from_auth(auth)
     if User.exists?(:uid => auth['uid'])
-      user = User.find_by(provider: auth['provider'], uid: auth['uid'])
-    else
-      user = User.new
-      user_info  = auth['extra']['raw_info']['user']
-      user_creds = auth['credentials']
-      user.provider      = auth['provider']
-      user.uid           = auth['uid']
-      user.first_name    = user_info['firstName']
-      user.last_name     = user_info['lastName']
-      user.gender        = user_info['gender']
-      user.age           = user_info['age']
-      user.height        = user_info['height']
-      user.token         = user_creds['token']
-      user.refresh_token = user_creds['refresh_token']
-      user.save
+      return User.find_by(provider: auth['provider'], uid: auth['uid'])
     end
-    user
+    create_new_user(auth)
   end
 
   def generate_auth_token
@@ -34,6 +20,25 @@ class User < ApplicationRecord
 
   def invalidate_auth_token
     self.update_columns(auth_token: nil)
+  end
+
+  private
+
+  def self.create_new_user(auth)
+    user = User.new
+    user_info  = auth['extra']['raw_info']['user']
+    user_creds = auth['credentials']
+    user.provider      = auth['provider']
+    user.uid           = auth['uid']
+    user.first_name    = user_info['firstName']
+    user.last_name     = user_info['lastName']
+    user.gender        = user_info['gender']
+    user.age           = user_info['age']
+    user.height        = user_info['height']
+    user.token         = user_creds['token']
+    user.refresh_token = user_creds['refresh_token']
+    user.save
+    user
   end
 
 end
