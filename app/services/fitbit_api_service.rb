8f32364808687bc037b3f8aa3e77d18d8dfe5e5d
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-05 09:01:12 -0700

    slight refactor on fitbit_api_service

--- app/services/fitbit_api_service.rb
+++ app/services/fitbit_api_service.rb
@@ -18,27 +18,31 @@ class FitbitApiService
   end
 
   def get_sleep_info(thirty_days_ago)
-    response = @conn.get("/1.2/user/#{@user.uid}/sleep/list.json?afterDate=#{thirty_days_ago}&sort=desc&offset=0&limit=30")
+    response = get("/1.2/user/#{@user.uid}/sleep/list.json?afterDate=#{thirty_days_ago}&sort=desc&offset=0&limit=30")
     sleep_info = JSON.parse(response.body, symbolize_names: true)[:sleep]
     Sleep.create_sleep_info(sleep_info, @user)
   end
 
   def get_activity_info
-    response = @conn.get("/1/user/#{@user.uid}/activities/activityCalories/date/today/30d.json")
+    response = get("/1/user/#{@user.uid}/activities/activityCalories/date/today/30d.json")
     activity_info = JSON.parse(response.body)["activities-activityCalories"]
     Activity.create_activity_entries(activity_info, @user)
   end
 
   def get_heart_info
-    response = @conn.get("/1/user/#{@user.uid}/activities/heart/date/today/30d.json")
+    response = get("/1/user/#{@user.uid}/activities/heart/date/today/30d.json")
     heart_info = JSON.parse(response.body)["activities-heart"]
     Heart.create_heart_info(heart_info, @user)
   end
 
   def get_body_info(current_day)
-    response = @conn.get("/1/user/#{@user.uid}/body/log/weight/date/#{current_day}/30d.json")
+    response = get("/1/user/#{@user.uid}/body/log/weight/date/#{current_day}/30d.json")
     body_info = JSON.parse(response.body)["weight"]
     Body.create_body_info(body_info, @user)
+  end
+
+  def get(url)
+    @conn.get(url)
   end
 
 end
