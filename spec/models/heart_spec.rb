0db267b1d67b0db0dd2a646e62da3846632406f1
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-05 07:01:57 -0700

    heart passing

--- spec/models/heart_spec.rb
+++ spec/models/heart_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Heart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(first_name: "Dead",
+                        last_name: "Mau5")
+  end
+
+  it {should belong_to(:user) }
+
+  it "creates heart_info from import file" do
+    heart_info = [{"dateTime" => "#{Date.today}",
+                   "value" => {"restingHeartRate" => 22}}]
+
+    Heart.create_heart_info(heart_info, @user)
+
+    heart = Heart.first
+
+    expect(Heart.count).to eq(1)
+    expect(heart.date).to eq(Date.today)
+    expect(heart.resting_heart_rate).to eq(22)
+  end
 end
