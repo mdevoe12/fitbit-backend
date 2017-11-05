61cb5ecab6c358e9d2ddd50786ede9a1b7ca3f85
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-05 07:54:59 -0700

    building out user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,10 +10,46 @@ RSpec.describe User, type: :model do
   it "should find existing user from auth" do
     user = User.create(uid: "test123")
 
-    auth = {"uid" => "test123" } 
+    auth = {"uid" => "test123" }
 
     result = User.find_or_create_from_auth(auth)
 
     expect(result).to eq(user)
+  end
+
+  it "should create new user if not found" do
+    auth = {
+      "provider" => "fitbit",
+      "uid" => "321test",
+      "credentials" => {
+        "token" => "123",
+        "refresh_token" => "432"
+      },
+      "extra" => {
+        "raw_info" => {
+          "user" => {
+            "firstName" => "Jack",
+            "lastName" => "Johnson",
+            "gender" => "male",
+            "age" => 99,
+            "height" => 72
+          }
+        }
+      }
+    }
+
+    User.create_new_user(auth)
+
+    user = User.first
+
+    expect(user.first_name).to eq("Jack")
+    expect(user.last_name).to eq("Johnson")
+    expect(user.provider).to eq("fitbit")
+    expect(user.uid).to eq("321test")
+    expect(user.gender).to eq("male")
+    expect(user.age).to eq(99)
+    expect(user.height).to eq(72)
+    expect(user.token).to eq("123")
+    expect(user.refresh_token).to eq("432")
   end
 end
