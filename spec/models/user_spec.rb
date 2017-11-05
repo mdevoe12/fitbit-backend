24637f050196b27dbe2635d29ca6f266245f433a
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-05 07:37:56 -0700

    slight refactor of User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_many(:hearts) }
+  it {should have_many(:bodies) }
+  it {should have_many(:activities) }
+  it {should have_many(:sleeps) }
+
+
+  it "should find existing user from auth" do
+    user = User.create(uid: "test123")
+
+    auth = {"uid" => "test123" } 
+
+    result = User.find_or_create_from_auth(auth)
+
+    expect(result).to eq(user)
+  end
 end
