246b853678e5acb738ef1223d0d3b72faa242b6f
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-05 23:13:42 -0700

    additional tests

--- spec/services/fitbit_api_service_spec.rb
+++ spec/services/fitbit_api_service_spec.rb
@@ -15,15 +15,45 @@ RSpec.describe FitbitApiService do
       expect(service.user).to eq(user)
     end
 
-    it "test import_thirty_day_data" do
+    it "returns sleep_url" do
       user = User.create(token: "123")
       service = FitbitApiService.new(user)
-      # allow(service).to receive(get_sleep_info).and_return(true)
-      # allow(service).to receive(get_activity_info).and_return(true)
-      # allow(service).to receive(get_heart_info).and_return(true)
-      # allow(service).to receive(get_body_info).and_return(true)
-      allow(service).to receive(import_thirty_day_data).and_return(true)
 
+      result = service.sleep_url
+      expected = "/1.2/user//sleep/list.json?afterDate=2017-10-06&sort=desc&offset=0&limit=30"
+
+      expect(result).to eq(expected)
+    end
+
+    it "returns activity_url" do
+      @user = User.create(token: "123", uid: '311')
+      service = FitbitApiService.new(@user)
+
+      result = service.activity_url
+      expected = "/1/user/#{@user.uid}/activities/activityCalories/date/today/30d.json"
+
+      expect(result).to eq(expected)
+    end
+
+    it "returns heart_url" do
+      @user = User.create(token: "123", uid: '311')
+      service = FitbitApiService.new(@user)
+
+      result = service.heart_url
+      expected = "/1/user/#{@user.uid}/activities/heart/date/today/30d.json"
+
+      expect(result).to eq(expected)
+    end
+
+    it "returns body_url" do
+      @user = User.create(token: "123", uid: '311')
+      service = FitbitApiService.new(@user)
+      current_day = Date.today
+
+      result = service.body_url
+      expected = "/1/user/#{@user.uid}/body/log/weight/date/#{current_day}/30d.json"
+
+      expect(result).to eq(expected)
     end
   end
 end
