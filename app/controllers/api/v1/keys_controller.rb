7746e85a002760d723ab88e932fa1d5edbf098ab
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-06 05:00:59 -0700

    added skip before action for verify authenticity token

--- app/controllers/api/v1/keys_controller.rb
+++ app/controllers/api/v1/keys_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::KeysController < ApplicationController
 
   skip_before_action :require_login!, only: [:index]
+  skip_before_filter :verify_authenticity_token, :only => [:index]
 
   def index
     if authenticate_url(request)
