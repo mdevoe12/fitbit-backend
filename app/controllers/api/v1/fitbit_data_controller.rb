a6a65aacbf29e2e17ef2d8eaf935f0290231ecb7
Matt DeVoe <matthew.devoe@gmail.com>
2017-11-05 22:17:15 -0700

    updates

--- app/controllers/api/v1/fitbit_data_controller.rb
+++ app/controllers/api/v1/fitbit_data_controller.rb
@@ -1,11 +1,14 @@
 class Api::V1::FitbitDataController < ApplicationController
 
+  skip_before_action :require_login!, only: [:index]
+
+
   def index
-    data = []
+    data_sleepHeartRate = []
     30.times do |i|
       date = "#{Date.today - i}"
       sleep_day_info = User.first.sleeps.find_by(date_of_wakeup: date)
-      data << {
+      data_sleepHeartRate << {
        date: date,
        heart_rate: User.first.hearts.find_by(date: date).resting_heart_rate,
        rem: sleep_day_info.rem_minutes,
@@ -14,7 +17,37 @@ class Api::V1::FitbitDataController < ApplicationController
        wake: sleep_day_info.wake_minutes,
        }
     end
+    data_weightActivity = []
+    30.times do |i|
+      date = "#{Date.today - i}"
+      data_weightActivity << {
+       date: date,
+       rem: User.first.sleeps.find_by(date_of_wakeup: date).rem_minutes,
+       caloriesOut: User.first.activities.find_by(date: date).active_calories_out
+       }
+    end
+    data = [data_sleepHeartRate.reverse, data_sleepHeartRate.reverse]
     render json: data
+  end
+
+  def find_weight(date)
+    if (User.first.bodies.find_by!(date: date)).success?
+      return User.first.bodies.find_by(date: date).weight
+    end
+    return 0
+  end
+
+  def show
+    data_weightActivity = []
+    30.times do |i|
+      date = "#{Date.today - i}"
+      data << {
+       date: date,
+       weight: User.first.bodies.find_by(date: date).weight,
+       caloriesOut: User.first.activities.find_by(date: date).active_calories_out
+       }
+    end
+    render json: data.reverse
   end
 
 end
